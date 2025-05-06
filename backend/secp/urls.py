488aa449275b2ee16e118d43cfaf86336da188ac
Leonardo <90990794+LeonardoJose21@users.noreply.github.com>
2025-05-06 06:19:37 -0500

    Update urls.py

--- backend/secp/urls.py
+++ backend/secp/urls.py
@@ -30,5 +30,6 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/", include("api.urls"))
+    path("api/", include("api.urls")),
+    path("", TemplateView.as_view(template_name="index.html"), name="react_app"),
 ]
