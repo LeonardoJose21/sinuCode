d02fc3758e1b250996412533a576660663f2143f
Leonardo Pastrana <leopastra2004@gmail.com>
2025-05-06 05:42:29 -0500

    eliminar innecesario

--- backend/secp/urls.py
+++ backend/secp/urls.py
@@ -30,6 +30,5 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/", include("api.urls")),
-    path('', TemplateView.as_view(template_name='index.html')),  # React
+    path("api/", include("api.urls"))
 ]
