dd4b835a12236cf0cc5b7a1f883a957e2b297198
Leonardo <90990794+LeonardoJose21@users.noreply.github.com>
2025-05-07 09:35:59 -0500

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-set -o errexit
-
-# Go to the root of the project to install dependencies
-pip install -r requirements.txt
-
-# Run collectstatic and migrate from the current directory (which contains manage.py)
-python manage.py collectstatic --no-input
-python manage.py migrate
+backend > $build.sh
+  set -o errexit
+  
+  # Go to the root of the project to install dependencies
+  pip install -r requirements.txt
+  
+  # Run collectstatic and migrate from the current directory (which contains manage.py)
+  python manage.py collectstatic --no-input
+  python manage.py migrate
